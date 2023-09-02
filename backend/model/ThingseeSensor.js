78614102262db3b3a5f211a4955bc1d408e2aedd
Shaikchapal <shaikchapal@hotmail.com>
2023-09-03 02:58:20 +0600

    added thingseeSensor

---
+++ backend/model/ThingseeSensor.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const thingseeSensorSchema = mongoose.Schema({
+  in: {
+    type: Number,
+    default: 0,
+  },
+  out: {
+    type: Number,
+    default: 0,
+  },
+  totalIn: {
+    type: Number,
+    default: 0,
+  },
+  totalOut: {
+    type: Number,
+    default: 0,
+  },
+  historicalIn: {
+    type: Number,
+    default: 0,
+  },
+  historicalOut: {
+    type: Number,
+    default: 0,
+  },
+  amountIn: {
+    type: Number,
+    default: 0,
+  },
+  carbonDioxide: {
+    type: Number,
+    default: 0,
+  },
+  tvoc: {
+    type: Number,
+    default: 0,
+  },
+  temp: {
+    type: Number,
+    default: 0,
+  },
+  humd: {
+    type: Number,
+    default: 0,
+  },
+  airp: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const ThingseeSensor = mongoose.model("ThingseeSensor", thingseeSensorSchema);
+
+module.exports = ThingseeSensor;
